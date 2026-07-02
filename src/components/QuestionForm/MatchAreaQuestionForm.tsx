6a1dd3b772da6ce072519a08585f008935052ba1
Mikołaj Kuranowski <mkuranowski@gmail.com>
2026-07-02 20:57:33 +0200

    Show Match question matches

--- src/components/QuestionForm/MatchAreaQuestionForm.tsx
+++ src/components/QuestionForm/MatchAreaQuestionForm.tsx
@@ -1,11 +1,26 @@
 // SPDX-FileCopyrightText: 2026 Mikołaj Kuranowski
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import { useSignals } from "@preact/signals-react/runtime";
+import { FormControl } from "react-bootstrap";
 import { type MatchAreaQuestion } from "../../model/question/index.ts";
 import BinaryAnswerButtons from "./common/BinaryAnswerButtons.tsx";
 import CandidateSelector from "./common/CandidateSelector.tsx";
 import CommonButtons from "./common/CommonButtons.tsx";
 import PositionSelector from "./common/PositionSelector.tsx";
+
+export function MatchedArea({ q }: { q: MatchAreaQuestion }) {
+    useSignals();
+    const matched = q.seekersMatch.value?.properties;
+    const name = matched?.name ?? matched?.id ?? "";
+    return (
+        <FormControl
+            type="text"
+            value={name}
+            disabled
+        />
+    );
+}
 
 export default function MatchAreaQuestionForm({
     q,
@@ -16,7 +31,9 @@ export default function MatchAreaQuestionForm({
 }) {
     return (
         <>
-            <CandidateSelector q={q} className="mb-2" />
+            <CandidateSelector q={q} className="mb-2">
+                <MatchedArea q={q} />
+            </CandidateSelector>
             <PositionSelector q={q} className="mb-2" />
             <CommonButtons q={q} index={index}>
                 <BinaryAnswerButtons q={q} />
--- src/components/QuestionForm/MatchPointQuestionForm.tsx
+++ src/components/QuestionForm/MatchPointQuestionForm.tsx
@@ -1,12 +1,26 @@
 // SPDX-FileCopyrightText: 2026 Mikołaj Kuranowski
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import { useSignals } from "@preact/signals-react/runtime";
+import { FormControl } from "react-bootstrap";
 import { type MatchPointQuestion } from "../../model/question/index.ts";
 import BinaryAnswerButtons from "./common/BinaryAnswerButtons.tsx";
 import CandidateSelector from "./common/CandidateSelector.tsx";
 import CommonButtons from "./common/CommonButtons.tsx";
 import PositionSelector from "./common/PositionSelector.tsx";
 
+export function MatchedPoint({ q }: { q: MatchPointQuestion }) {
+    useSignals();
+    const matched = q.seekersMatch.value?.properties;
+    const name = matched?.name ?? matched?.id ?? "";
+    return (
+        <FormControl
+            type="text"
+            value={name}
+            disabled
+        />
+    );
+}
 export default function MatchPointQuestionForm({
     q,
     index,
@@ -16,7 +30,9 @@ export default function MatchPointQuestionForm({
 }) {
     return (
         <>
-            <CandidateSelector q={q} className="mb-2" />
+            <CandidateSelector q={q} className="mb-2">
+                <MatchedPoint q={q} />
+            </CandidateSelector>
             <PositionSelector q={q} className="mb-2" />
             <CommonButtons q={q} index={index}>
                 <BinaryAnswerButtons q={q} />
